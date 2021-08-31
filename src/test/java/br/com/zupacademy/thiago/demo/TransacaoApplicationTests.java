12b74c721b0ae9f46d636324e515355bcfed3cfd
ThiagoZup <thiago.reis@zup.com.br>
2021-08-31 14:23:13 -0300

    setup

---
+++ src/test/java/br/com/zupacademy/thiago/demo/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.thiago.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
